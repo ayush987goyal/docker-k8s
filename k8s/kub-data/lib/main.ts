2ca5b0a9a36391b43bf40235b58bdaa28e547a52
Ayush Goyal <goyalayu@amazon.com>
2020-11-22 12:24:44 +0530

    add persistent volume

--- k8s/kub-data/lib/main.ts
+++ k8s/kub-data/lib/main.ts
@@ -1,28 +1,79 @@
 import { Construct } from 'constructs';
 import * as k from 'cdk8s';
-import * as kplus from 'cdk8s-plus-17';
+
+import {
+  KubeService,
+  KubeDeployment,
+  KubePersistentVolume,
+  KubePersistentVolumeClaim,
+} from '../imports/k8s';
 
 export class MyChart extends k.Chart {
   constructor(scope: Construct, id: string, props: k.ChartProps = {}) {
     super(scope, id, props);
 
-    const storyDeployment = new kplus.Deployment(this, 'story-deployment', {
-      containers: [
-        {
-          image: 'ayush987goyal/kub-data-demo:1',
-          volumeMounts: [
-            {
-              path: '/app/story',
-              volume: kplus.Volume.fromEmptyDir('story-volume'),
-            },
-          ],
-        },
-      ],
+    const label = { app: 'kub-data-app' };
+    const persistentVolumeName = 'host-pv';
+    const persistentVolumeClaimName = 'host-pvc';
+
+    new KubePersistentVolume(this, 'story-persistent-volume', {
+      metadata: { name: persistentVolumeName },
+      spec: {
+        capacity: { storage: '1Gi' },
+        volumeMode: 'Filesystem',
+        storageClassName: 'standard',
+        accessModes: ['ReadWriteOnce'],
+        hostPath: { path: '/data', type: 'DirectoryOrCreate' },
+      },
     });
 
-    storyDeployment.expose(80, {
-      targetPort: 3000,
-      serviceType: kplus.ServiceType.LOAD_BALANCER,
+    new KubePersistentVolumeClaim(this, 'story-persistent-volume-claim', {
+      metadata: { name: persistentVolumeClaimName },
+      spec: {
+        volumeName: persistentVolumeName,
+        accessModes: ['ReadWriteOnce'],
+        storageClassName: 'standard',
+        resources: {
+          requests: { storage: '1Gi' },
+        },
+      },
+    });
+
+    new KubeService(this, 'story-service', {
+      spec: {
+        type: 'LoadBalancer',
+        ports: [{ port: 80, targetPort: 3000 }],
+        selector: label,
+      },
+    });
+
+    new KubeDeployment(this, 'story-deployment', {
+      spec: {
+        replicas: 2,
+        selector: {
+          matchLabels: label,
+        },
+        template: {
+          metadata: { labels: label },
+          spec: {
+            containers: [
+              {
+                name: 'story',
+                image: 'ayush987goyal/kub-data-demo:1',
+                volumeMounts: [
+                  { mountPath: '/app/story', name: 'story-volume' },
+                ],
+              },
+            ],
+            volumes: [
+              {
+                name: 'story-volume',
+                persistentVolumeClaim: { claimName: persistentVolumeClaimName },
+              },
+            ],
+          },
+        },
+      },
     });
   }
 }
