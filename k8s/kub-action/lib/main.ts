f43d1cf4e7aaa937f031fc007db6ffc2bc2085e5
Ayush Goyal <goyalayu@amazon.com>
2020-11-22 09:17:21 +0530

    add basic deployment throug kplus

--- k8s/kub-action/lib/main.ts
+++ k8s/kub-action/lib/main.ts
@@ -1,12 +1,32 @@
 import { Construct } from 'constructs';
-import { App, Chart, ChartProps } from 'cdk8s';
+import * as k from 'cdk8s';
+import * as kplus from 'cdk8s-plus-17';
 
-export class MyChart extends Chart {
-  constructor(scope: Construct, id: string, props: ChartProps = {}) {
+export class MyChart extends k.Chart {
+  constructor(scope: Construct, id: string, props: k.ChartProps = {}) {
     super(scope, id, props);
+
+    const deployment = new kplus.Deployment(this, 'Deployment', {
+      containers: [
+        {
+          name: 'kub-first-app',
+          image: 'ayush987goyal/kub-first-app',
+          port: 8080,
+          liveness: kplus.Probe.fromHttpGet('/', {
+            port: 8080,
+            periodSeconds: k.Duration.seconds(10),
+            initialDelaySeconds: k.Duration.seconds(5),
+          }),
+        },
+      ],
+    });
+
+    deployment.expose(8080, {
+      serviceType: kplus.ServiceType.LOAD_BALANCER,
+    });
   }
 }
 
-const app = new App();
+const app = new k.App();
 new MyChart(app, 'kub-action');
 app.synth();
