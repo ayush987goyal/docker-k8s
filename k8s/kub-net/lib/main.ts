505630e23d285bd7ba9b278c9db77131c4946578
Ayush Goyal <goyalayu@amazon.com>
2020-11-22 14:46:33 +0530

    add dummy users api

--- k8s/kub-net/lib/main.ts
+++ k8s/kub-net/lib/main.ts
@@ -1,19 +1,46 @@
 import { Construct } from 'constructs';
 import * as k from 'cdk8s';
 
-import {
-  KubeService,
-  KubeConfigMap,
-  KubeDeployment,
-  KubePersistentVolume,
-  KubePersistentVolumeClaim,
-} from '../imports/k8s';
+import { KubeService, KubeDeployment } from '../imports/k8s';
+
+class UsersService extends Construct {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    const label = { app: 'kub-net-users' };
+
+    new KubeService(this, 'users-service', {
+      spec: {
+        selector: label,
+        type: 'LoadBalancer',
+        ports: [{ port: 8080, targetPort: 8080 }],
+      },
+    });
+
+    new KubeDeployment(this, 'users-deployment', {
+      spec: {
+        selector: { matchLabels: label },
+        template: {
+          metadata: { labels: label },
+          spec: {
+            containers: [
+              {
+                name: 'users',
+                image: 'ayush987goyal/kube-net-demo-users',
+              },
+            ],
+          },
+        },
+      },
+    });
+  }
+}
 
 export class MyChart extends k.Chart {
   constructor(scope: Construct, id: string, props: k.ChartProps = {}) {
     super(scope, id, props);
 
-    const label = { app: 'kub-net-app' };
+    new UsersService(this, 'users');
   }
 }
 
