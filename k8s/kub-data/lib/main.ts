0f47580e39633a009e03e46e261ce18f36ed7e7a
Ayush Goyal <goyalayu@amazon.com>
2020-11-22 11:15:51 +0530

    add basic deployment

--- k8s/kub-data/lib/main.ts
+++ k8s/kub-data/lib/main.ts
@@ -1,10 +1,24 @@
 import { Construct } from 'constructs';
 import * as k from 'cdk8s';
-// import * as kplus from 'cdk8s-plus-17';
+import * as kplus from 'cdk8s-plus-17';
 
 export class MyChart extends k.Chart {
   constructor(scope: Construct, id: string, props: k.ChartProps = {}) {
     super(scope, id, props);
+
+    const storyDeployment = new kplus.Deployment(this, 'story-deployment', {
+      containers: [
+        {
+          image: 'ayush987goyal/kub-data-demo',
+          port: 3000,
+        },
+      ],
+    });
+
+    storyDeployment.expose(80, {
+      targetPort: 3000,
+      serviceType: kplus.ServiceType.LOAD_BALANCER,
+    });
   }
 }
 
